234b01b7bbb48292c2311ab2e49ac0559432170b
Ciffelia <mc.prince.0203@gmail.com>
2024-04-06 09:22:48 +0000

    fix links in docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 //! A tiny [tower] ([hyper], [axum], [warp] etc) service to generate a random id for each
 //! incoming request.
 //!
-//! This crate is a fork of [`tower-request-id`](https://docs.rs/tower-request-id) crate that uses Nano ID instead of ULID.
+//! This crate is a fork of [tower-request-id] crate that uses Nano ID instead of ULID.
+//!
+//! [tower]: https://crates.io/crates/tower
+//! [hyper]: https://crates.io/crates/hyper
+//! [axum]: https://crates.io/crates/axum
+//! [warp]: https://crates.io/crates/warp
+//! [tower-request-id]: https://crates.io/crates/tower-request-id
 
 use std::{
     fmt,
